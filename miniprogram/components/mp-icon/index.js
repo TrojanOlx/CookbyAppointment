5e81bbbab685fcbf1d7100d3b0f3bb9e513ca0e0
TrojanOlx <email@oulongxing.com>
2026-08-19 20:43:51 +0800

    fix: reduce mini program package size

---
+++ miniprogram/components/mp-icon/index.js
@@ -0,0 +1,20 @@
+Component({
+  properties: {
+    extClass: {
+      type: String,
+      value: ''
+    },
+    icon: {
+      type: String,
+      value: ''
+    },
+    size: {
+      type: Number,
+      value: 20
+    },
+    color: {
+      type: String,
+      value: '#000000'
+    }
+  }
+});
